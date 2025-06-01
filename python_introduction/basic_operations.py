99e1379ffd25e67fe1b76299db12a52ad56e4a95
Joseph Siwiti <josephsiwiti@gmail.com>
2025-06-01 11:24:47 -0700

    added script that performs basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+number1 = 10
+number2 = 5
+
+the_sum = number2 + number1
+difference = number1 - number2
+product = number1*number2
+
+print("Addition of ",number2,"and ",number1," is ",the_sum)
+print("Subtraction of ",number1," and ",number2," is ",difference)
+print("Multiplication of",number1," and ",number2," is ",product)
+
